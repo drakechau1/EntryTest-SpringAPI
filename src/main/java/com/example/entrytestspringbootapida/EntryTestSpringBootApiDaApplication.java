efddfa0b2d05b759339d106bcd029ff9df1cd1d8
duccm <ducchau.dev@gmail.com>
2023-11-25 09:16:58 +0700

    init project

---
+++ src/main/java/com/example/entrytestspringbootapida/EntryTestSpringBootApiDaApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrytestspringbootapida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryTestSpringBootApiDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryTestSpringBootApiDaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/entrytestspringbootapida/EntryTestSpringBootApiDaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entrytestspringbootapida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryTestSpringBootApiDaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
