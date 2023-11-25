223c04fcb5846bcb2a23349a76ad33e9e852fd1c
duccm <ducchau.dev@gmail.com>
2023-11-25 12:37:17 +0700

    update: TaskController

---
+++ src/main/java/com/example/entrytestspringbootapida/api/controller/TaskController.java
@@ -0,0 +1,42 @@
+package com.example.entrytestspringbootapida.api.controller;
+
+import com.example.entrytestspringbootapida.api.model.Task;
+import com.example.entrytestspringbootapida.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> GetAllTask() {
+        return taskService.GetAllTask();
+    }
+
+    @PostMapping
+    public Task CreateTask(@RequestBody Task task) {
+        return taskService.CreateTask(task);
+    }
+
+    @GetMapping("/{id}")
+    public Task GetTaskById(@PathVariable Long id) {
+        return taskService.GetTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task UpdateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.UpdateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void DeleteTask(@PathVariable Long id) {
+        taskService.DeleteTask(id);
+    }
+}
